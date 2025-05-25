1c00d171d123fb27d02c1bf97a2f55a02a7ff8df
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-25 22:42:10 +0600

    Insert slightings table data

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -38,7 +38,15 @@ VALUES
 ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
 ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
  
- 
+
+-- insert slightings table data
+
+INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes)
+VALUES
+(1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+(2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+(3, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+(1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', NULL);
 
 
 
