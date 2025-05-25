e774f84d6ce86ba94bdb881f6dfcaddfcd7534de
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-25 22:49:51 +0600

    Solved : Problem - 3

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -54,6 +54,16 @@ VALUES
 INSERT INTO rangers(name, region) VALUES('Derek Fox','Coastal Plains');
 
 
+-- Problem - 2:
+SELECT count(DISTINCT species_id) AS unique_species_count 
+FROM sightings;
+
+
+
+
+
+
+
 
 
 
