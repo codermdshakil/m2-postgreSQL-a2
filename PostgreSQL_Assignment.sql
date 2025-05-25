9380e648b96ddb5282636d29142a4d6c431d9d99
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-25 22:34:22 +0600

    Create tables : rangers, species, sightings

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,7 +1,32 @@
 -- Active: 1747835496638@@127.0.0.1@5432@ph
 
+-- creating tables
+CREATE Table rangers(
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(70) NOT NUll
+);
+
+CREATE Table species(
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE,
+    conservation_status VARCHAR(70) CHECK(
+        conservation_status IN ('Endangered', 'Vulnerable')
+    )
+);
+
+CREATE TABLE sightings (
+    sighting_id SERIAL PRIMARY KEY,
+    ranger_id INTEGER REFERENCES rangers(ranger_id) NOT NULL ,
+    species_id INTEGER REFERENCES species(species_id) NOT NULL ,
+    sighting_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    location VARCHAR(100) NOT NULL,
+    notes TEXT
+);
+
 
 
 
 -- Problem - 1:
-
